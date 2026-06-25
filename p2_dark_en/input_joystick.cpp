22b9f06f6de2c2e18abfe65c6ab22157c8d83419
avengah <avengah6@gmail.com>
2026-06-25 19:22:01 +0100

    Reduce target in crosshairs wait time to 100ms

--- p2_dark_en/input_joystick.cpp
+++ p2_dark_en/input_joystick.cpp
@@ -1,9 +1,9 @@
 /*
 The MIT License (MIT)
-Copyright © 2026 Matt Wells
+Copyright ¬© 2026 Matt Wells
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this
-software and associated documentation files (the ìSoftwareî), to deal in the
+software and associated documentation files (the ‚ÄúSoftware‚Äù), to deal in the
 Software without restriction, including without limitation the rights to use, copy,
 modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 and to permit persons to whom the Software is furnished to do so, subject to the
@@ -12,7 +12,7 @@ following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED ìAS ISî, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+THE SOFTWARE IS PROVIDED ‚ÄúAS IS‚Äù, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
 INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
 PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
 HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
@@ -70,7 +70,7 @@ LONGLONG target_in_crosshairs_roll_duration = 0;
 void Initiate_Target_In_Crosshairs_Wait_Time() {
 	static bool run_once = false;
 	if (!run_once) {
-		target_in_crosshairs_roll_wait_time = (LONGLONG)300 * Frequency.QuadPart / 1000LL;//ms to ticks
+		target_in_crosshairs_roll_wait_time = (LONGLONG)100 * Frequency.QuadPart / 1000LL;//ms to ticks
 		run_once = true;
 	}
 }
